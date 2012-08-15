6968d1df3760d02415eaf1c987d6414de44e5006
JuanJose "JJ" Galvez <jj.galvez@inktank.com>
2012-08-15 11:00:41 -0700

    move the counter!

--- fio-runner.sh
+++ fio-runner.sh
@@ -11,10 +11,10 @@ TIME=`date +%H-%M-%S`
 RESULT_PATH="./results/${YEAR}/${MONTH}/${DAY}"
 mkdir -p ${RESULT_PATH}
 
+COUNTER=1
 until [ $COUNTER -gt $NUMBER_OF_TIMES_TO_RUN_EACH_JOB ]; do
   for t in `ls -1 enabled-tests/`
   do
-    COUNTER=1
     echo "===================="
       echo "Running: ${t}"
       echo "Pass Number: ${COUNTER}"
