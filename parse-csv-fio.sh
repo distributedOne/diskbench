77ff44c0fa4a09a26b11406d768774b104e3e34c
Ignace Mouzannar <ignace.mouzannar@enovance.com>
2012-09-07 21:41:49 +0000

    updated to fit new naming

--- parse-csv-fio.sh
+++ parse-csv-fio.sh
@@ -2,34 +2,26 @@
 
 ROOT_PATH="results"
 
-for year in `ls -1 $ROOT_PATH/`;
+echo "Test Name,Read IOPS,WRITE IOPS,READ BW, WRITE BW"
+for runset in `ls -1 $ROOT_PATH/`;
 do
-  YEAR_PATH=$ROOT_PATH/$year
-  for month in `ls -1 $YEAR_PATH/`;
+  ROOT_PATH="${ROOT_PATH}/${runset}"
+  for pass in `ls -1 $ROOT_PATH/fio-* | awk -F'pass-' {'print \$2'} | uniq | sort | uniq`;
   do
-    MONTH_PATH=$YEAR_PATH/$month
-    for day in `ls -1 $MONTH_PATH/`;
+    for file in `ls -1 $ROOT_PATH/fio-*-$pass`;
     do
-      DAY_PATH=$MONTH_PATH/$day
-      for runset in `ls -1 $DAY_PATH | awk -F- {'print \$1"-"\$2"-"\$3'} | uniq | sort | uniq`;
-      do
-        for pass in `ls -1 $DAY_PATH/$runset-* | awk -F'pass-' {'print \$2'} | uniq | sort | uniq`;
-        do
-          for file in `ls -1 $DAY_PATH/$runset-*-$pass`;
-          do
-            JOB=`head -n 1 ${file} | awk -F: {'print \$1'}`
-            echo -n "${JOB},";
-            READ_IOPS=`grep iops ${file}| grep read | awk -F'iops=' {'print \$2'} | awk {'print \$1'}`
-            echo -n "${READ_IOPS},"
-            WRITE_IOPS=`grep iops ${file}| grep write | awk -F'iops=' {'print \$2'} | awk {'print \$1'}`
-            echo -n "${WRITE_IOPS},"
-            READ_BANDWIDTH=`grep READ ${file} | awk -F'maxb=' {'print \$2'} | awk -F, {'print \$1'}`
-            echo -n "${READ_BANDWIDTH},"
-            WRITE_BANDWIDTH=`grep WRITE ${file} | awk -F'maxb=' {'print \$2'} | awk -F, {'print \$1'}`
-            echo "${WRITE_BANDWIDTH}"
-          done
-        done
-      done
+      cat $ROOT_PATH/NAME
+      echo -n ","
+      JOB=`head -n 1 ${file} | awk -F: {'print \$1'}`
+      echo -n "${JOB},";
+      READ_IOPS=`grep iops ${file}| grep read | awk -F'iops=' {'print \$2'} | awk {'print \$1'}`
+      echo -n "${READ_IOPS},"
+      WRITE_IOPS=`grep iops ${file}| grep write | awk -F'iops=' {'print \$2'} | awk {'print \$1'}`
+      echo -n "${WRITE_IOPS},"
+      READ_BANDWIDTH=`grep READ ${file} | awk -F'maxb=' {'print \$2'} | awk -F, {'print \$1'}`
+      echo -n "${READ_BANDWIDTH},"
+      WRITE_BANDWIDTH=`grep WRITE ${file} | awk -F'maxb=' {'print \$2'} | awk -F, {'print \$1'}`
+      echo "${WRITE_BANDWIDTH}"
     done
   done
 done
